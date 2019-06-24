03489f8bf3e0121b2adb5c140c06db56865df57f
Kleyton Ramos <kleytonmatosramos@gmail.com>
2019-06-24 12:19:14 -0300

    add event click

--- js/map.js
+++ js/map.js
@@ -9,15 +9,13 @@ function init(){
           $select.append('<option value=' + value.key + '>' + value.munic + '</option>');
         });
         selected(data.municipios)
+        clicked(data.municipios)
       }
     });
   });
 }
 
 function selected(data){
-  // $('#geojs-32-mun').on('click', function() {
-  //   console.log($(this).find('g').attr("id"));
-  // });
   var selectedValueRadio;
   $("#radio").on('change', function () {
     selectedValueRadio = $("input[name='options']:checked").val();
@@ -67,7 +65,60 @@ function selected(data){
     }
   });
 }
-selected();
+
+function clicked(data){
+  var selectedValueRadio;
+  $("#radio").on('change', function () {
+    selectedValueRadio = $("input[name='options']:checked").val();
+    $('#list-munic option:first').prop('selected', true);
+    for (var i in data){
+      $("#" + data[i].key).css("fill", "#e7e8ea");
+      $("#agrupamentos").find('li').remove()
+    }
+    console.log(selectedValueRadio);
+  });
+
+  $('g').on('click', function() {
+    var munic_current = $(this).attr("id")
+    $("#list-munic").val($(this).attr("id")).change();
+    console.log(munic_current)
+
+    for (var i in data){
+      $("#" + data[i].key).css("fill", "#e7e8ea");
+      $("#agrupamentos").find('li').remove()
+    }
+    
+    for (var i in data){
+      if (data[i].key == munic_current) {
+        if(selectedValueRadio == "cluster"){
+          for (var j in data){
+            if (data[i].cluster == data[j].cluster) {
+              $("#" + data[j].key).css("fill", "#a1a1a1");
+              $("#" + munic_current).css("fill", "#ffba5a");
+              $("#agrupamentos").append("<li>" + data[j].munic +": "+ data[j].cluster+ "</li>");
+            }
+          }
+        }else if(selectedValueRadio == "regional"){
+          for (var j in data){
+            if (data[i].regional == data[j].regional) {
+              $("#" + data[j].key).css("fill", "#2d3091");
+              $("#" + munic_current).css("fill", "#ffba5a");
+              $("#agrupamentos").append("<li>" + data[j].munic +": "+ data[j].regional+ "</li>");
+            }
+          }
+        }else if(selectedValueRadio == "estadual"){
+          for (var j in data){
+            $("#" + data[j].key).css("fill", "#a1a1a1");
+            $("#" + munic_current).css("fill", "#ffba5a");
+          }
+        }else{
+          alert("Escolha um grupo primeiro!")
+        }
+      }
+    }
+  });
+}
+
 init();
 
 
